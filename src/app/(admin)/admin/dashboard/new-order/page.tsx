c9680115f97c4be57b666006ae0d45ce0eedc425
kislay-gupta <kislaygupta007@gmail.com>
2024-05-25 22:30:43 +0530

    feat: new order page

---
+++ src/app/(admin)/admin/dashboard/new-order/page.tsx
@@ -0,0 +1,12 @@
+import OrderTable from "@/components/admin/OrderTable";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <OrderTable title="New order" desc="new orders" />
+    </div>
+  );
+};
+
+export default page;
